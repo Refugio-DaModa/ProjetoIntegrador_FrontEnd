c790fabc098d40087559171d8581c26cee624f5c
Lucas Mendes dos Santos Mota <lucas.mendes0515@gmail.com>
2022-10-03 18:27:39 -0300

    Home

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -11,12 +11,12 @@ function Home(){
                     <Box paddingX={20} >
                         <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className="titulo">Seja bem vindo(a)!</Typography>
                         <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo">Veja nossos produtos!Qualidade aqui meu parceiro</Typography>
-                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo">Qualidade aqui meu parceiro!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className="titulo" >Qualidade aqui meu parceiro!</Typography>
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
                         </Box>
-                        <Button variant="outlined" className="botao">Ver Produtos</Button>
+                        <Button variant="outlined" className="botao" >Ver Produtos</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
