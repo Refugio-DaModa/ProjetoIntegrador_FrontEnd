e83ae093baff05bfe3dd853857e379bef09faf52
αℓєχ __ <alex.ka.freitas@gmail.com>
2022-10-04 00:08:50 -0300

    fix: alinhando imagem ao centro

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -27,7 +27,13 @@ function Sobre() {
           </Box>
         </Grid>
 
-        <Grid item xs={6} className="bg-login"><img src="https://i.imgur.com/DRARIhi.gif" alt="" /></Grid>
+        <Grid item xs={6} alignItems="center" >
+        <Box paddingX={20} paddingY={5}>
+            <img src="https://i.imgur.com/DRARIhi.gif" alt="imagem" />
+            
+        </Box>
+            
+        </Grid>
       </Grid>
 
 
@@ -52,7 +58,7 @@ function Sobre() {
               <Box display='flex' justifyContent='center' marginTop={2}>
               </Box>
 
-            <Box display="flex" textAlign="justify" marginTop={2}>
+            <Box display="flex" textAlign="justify" marginTop={2} >
             <Box marginRight={1}>
               <Typography variant='subtitle1'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</Typography>
             </Box>
