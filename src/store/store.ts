3a49526e6e45841ecfcfee77da474f99ac6389bc
Bruno Nogueira <bruno.nogueiratt@gmail.com>
2022-10-13 11:24:54 -0300

    criação do redux

---
+++ src/store/store.ts
@@ -0,0 +1,6 @@
+import { legacy_createStore as createStore } from "redux";
+import { tokensReducer } from "./tokens/tokensReducer";
+
+const store = createStore(tokensReducer)
+
+export default store
