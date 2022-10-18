7f8c1a5f5bdd5a6a96732f94a90c8ae91d78ff3b
Thais Oliveira <thaa.s.oliveira@hotmail.com>
2022-10-18 16:38:09 -0300

    Ajustes finais da tela Login

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-    baseURL: "https://refugiodamoda.onrender.com/"
+    baseURL: "https://refugiodamoda.herokuapp.com/" // refugiodamoda.onrender.com
 })
 
 export const login = async(url: any , dados:any, setDado:any) =>{
