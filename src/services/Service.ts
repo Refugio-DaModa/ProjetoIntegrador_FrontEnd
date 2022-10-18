d6aefbe665184ffd6a2b71ca6d8707dee64bdb90
Lucas Mendes dos Santos Mota <lucas.mendes0515@gmail.com>
2022-10-18 17:22:38 -0300

    estilizando a home

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const api = axios.create({
-    baseURL: "https://refugiodamoda.herokuapp.com/"
+    baseURL: "https://refugiodamoda.onrender.com/"
 })
 
 export const login = async(url: any , dados:any, setDado:any) =>{
